a648b9c123400e016f68a7a5993f50e559b39201
Killoran Lehenbauer <k.lehenbauer13@gmail.com>
2020-06-13 20:11:32 -0700

    App fully functional

--- client/src/components/EditExercise.js
+++ client/src/components/EditExercise.js
@@ -13,6 +13,20 @@ export default class EditExercise extends Component {
     }
 
     componentDidMount() {
+
+        axios.get(`http://localhost:5000/exercises/${this.props.match.params.id}`)
+      .then(res => {
+        this.setState({
+          username: res.data.username,
+          description: res.data.description,
+          duration: res.data.duration,
+          date: new Date(res.data.date)
+        })   
+      })
+      .catch(function (err) {
+        console.log(err);
+      })
+
         axios.get('http://localhost:5000/users')
             .then(res => {
                 if (res.data.length > 0) {
@@ -59,7 +73,7 @@ export default class EditExercise extends Component {
 
         console.log(exercise);
 
-        axios.post('http://localhost:5000/exercises/add', exercise)
+        axios.post(`http://localhost:5000/exercises/update/${this.props.match.params.id}`, exercise)
         .then(res => console.log(res.data));
 
         window.location = '/';
@@ -68,7 +82,7 @@ export default class EditExercise extends Component {
     render() {
         return (
             <div>
-      <h3>Create New Exercise Log</h3>
+      <h3>Edit Exercise Log</h3>
       <form onSubmit={this.onSubmit}>
         <div className="form-group"> 
           <label>Username: </label>
@@ -116,7 +130,7 @@ export default class EditExercise extends Component {
         </div>
 
         <div className="form-group">
-          <input type="submit" value="Create Exercise Log" className="btn btn-primary" />
+          <input type="submit" value="Edit Exercise Log" className="btn btn-primary" />
         </div>
       </form>
     </div>
