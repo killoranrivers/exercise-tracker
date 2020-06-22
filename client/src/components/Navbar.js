9e91d9997c76aa2f58d977921ed16100b93ba8b7
Killoran Lehenbauer <k.lehenbauer13@gmail.com>
2020-06-21 23:38:34 -0700

    Working on api routes

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+// import { Navbar } from 'react-bootstrap';
 
 export default class Navbar extends Component {
     render() {
         return (
           <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
             <Link to="/" className="navbar-brand">ExcerTracker</Link>
-            <div className="collapse navbar-collapse">
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+            <div className="collapse navbar-collapse" id="navbarResponsive">
             <ul className="navbar-nav mr-auto">
               <li className="navbar-item">
               <Link to="/" className="nav-link">Exercises</Link>
