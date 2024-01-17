370fde005299a02de80501332cac13404b6d3295
KillRiv <k.rivers121@gmail.com>
2024-01-16 22:18:39 -0600

    fixed more shit

--- server/server.js
+++ server/server.js
@@ -11,7 +11,11 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static("client/build"));
+    app.use(express.static(path.join(__dirname, "client/build/index.html")));
+} else {
+    app.use((req, res) =>
+        res.sendFile(path.join(__dirname, "../client/build/index.html"))
+    );
 }
 
 const uri = process.env.MONGODB_URI;
@@ -19,17 +23,13 @@ mongoose.connect(uri);
 const connection = mongoose.connection;
 connection.once('open', () => {
     console.log(`MongoDB database connection established!`);
-}); 
+});
 
 const exercisesRouter = require('./routes/exercises');
 const usersRouter = require('./routes/users');
 
 app.use('/exercises', exercisesRouter);
 app.use('/users', usersRouter);
-
-app.use((req, res) =>
-  res.sendFile(path.join(__dirname, "client/build/index.html"))
-);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}!`);
